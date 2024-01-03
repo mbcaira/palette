3d13f227fc71f1b7c4ab8ab479bc41cf64773c13
Michael Caira <mbcaira@gmail.com>
2024-01-03 00:56:57 -0500

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,18 @@
-use image::{io::Reader as ImageReader, GenericImageView, ImageError, Rgba};
-fn main() {
-    let _ = create_image_slices("image.png".to_string(), 4, true);
-}
+mod palette;
 
-#[derive(Debug)]
-struct ImageSlice {
-    pixels: Vec<Rgba<u8>>,
-    bounds: (u32, u32),
-}
+use image::{io::Reader as ImageReader, ImageError};
+use palette::{AverageSlice, SliceType};
 
-fn create_image_slices(
-    image_path: String,
-    num_of_slices: u32,
-    is_vertically_sliced: bool,
-) -> Result<(), ImageError> {
-    let img = ImageReader::open(image_path)?.decode()?;
-    let dimensions = img.dimensions();
+fn main() -> Result<(), ImageError> {
+    let image_path = "image.png".to_string();
+    let img: image::DynamicImage = ImageReader::open(image_path)?.decode()?;
+    let slices = palette::slice_image(&img, 4, SliceType::Vertical);
 
-    let slice_size = if is_vertically_sliced {
-        dimensions.0 / num_of_slices
-    } else {
-        dimensions.1 / num_of_slices
-    };
-
-    let mut image_slices = Vec::new();
-    for i in 0..num_of_slices {
-        image_slices.push(ImageSlice {
-            pixels: Vec::new(),
-            bounds: (i * slice_size, (i + 1) * slice_size),
-        })
+    let mut average = Vec::new();
+    for slice in slices {
+        average.push(palette::dominant_colour(slice))
     }
 
-    println!("{:?}", image_slices);
-
-    for pixel in img.pixels() {
-        let coord = if is_vertically_sliced {
-            pixel.0
-        } else {
-            pixel.1
-        };
-        let colour = pixel.2;
-        for slice in &mut image_slices {
-            if slice.bounds.0 <= coord && coord < slice.bounds.1 {
-                slice.pixels.push(colour);
-                break;
-            }
-        }
-    }
-
-    let mut average_slices: Vec<ImageSlice> = Vec::new();
-
-    for slice in &image_slices {
-        average_slices.push(dominant_colour_slice(slice))
-    }
-
-    println!("{:?}", average_slices);
-
+    println!("{:?}", average);
     Ok(())
-}
-
-fn dominant_colour_slice(slice: &ImageSlice) -> ImageSlice {
-    let mut average_r: u32 = 0;
-    let mut average_g: u32 = 0;
-    let mut average_b: u32 = 0;
-    let mut average_a: u32 = 0;
-
-    let slice_length: usize = slice.pixels.len();
-
-    for pixel in &slice.pixels {
-        average_r += pixel.0[0] as u32;
-        average_g += pixel.0[1] as u32;
-        average_b += pixel.0[2] as u32;
-        average_a += pixel.0[3] as u32;
-    }
-    ImageSlice {
-        pixels: vec![Rgba::<u8>::from([
-            (average_r / slice_length as u32) as u8,
-            (average_g / slice_length as u32) as u8,
-            (average_b / slice_length as u32) as u8,
-            (average_a / slice_length as u32) as u8,
-        ])],
-        bounds: slice.bounds,
-    }
-}
-
-#[allow(unused)]
-fn kmeans_colour_slice(slice: &[Rgba<u8>]) -> Rgba<u8> {
-    todo!()
 }
---
+++ src/palette.rs
@@ -0,0 +1,77 @@
+use image::{DynamicImage, GenericImageView, Rgba, SubImage};
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum SliceType {
+    Horizontal,
+    Vertical,
+}
+
+#[derive(Debug)]
+pub struct AverageSlice {
+    colour: Rgba<u8>,
+    bounds: ((u32, u32), (u32, u32)),
+}
+
+pub fn slice_image(
+    img: &DynamicImage,
+    num_sections: usize,
+    slice_type: SliceType,
+) -> Vec<SubImage<&DynamicImage>> {
+    let dimensions = img.dimensions();
+
+    let mut image_slices = Vec::new();
+
+    for i in 0..num_sections {
+        match slice_type {
+            SliceType::Vertical => {
+                let size = dimensions.0 / num_sections as u32;
+                println!("{}", dimensions.1);
+                image_slices.push(img.view(i as u32 * size, 0, size, dimensions.1));
+            }
+            SliceType::Horizontal => {
+                let size = dimensions.1 / num_sections as u32;
+                image_slices.push(img.view(0, i as u32 * size, size, dimensions.0));
+            }
+        };
+    }
+
+    image_slices
+}
+
+pub fn dominant_colour(slice: SubImage<&DynamicImage>) -> AverageSlice {
+    let mut average_r: u32 = 0;
+    let mut average_g: u32 = 0;
+    let mut average_b: u32 = 0;
+    let mut average_a: u32 = 0;
+
+    let mut slice_length = 0;
+    for pixel in slice.pixels() {
+        average_r += pixel.2[0] as u32;
+        average_g += pixel.2[1] as u32;
+        average_b += pixel.2[2] as u32;
+        average_a += pixel.2[3] as u32;
+
+        slice_length += 1;
+    }
+    let bounds = (
+        (slice.bounds().0, slice.bounds().1),
+        (
+            slice.bounds().0 + slice.bounds().2,
+            slice.bounds().1 + slice.bounds().3,
+        ),
+    );
+    AverageSlice {
+        colour: Rgba::<u8>::from([
+            (average_r / slice_length as u32) as u8,
+            (average_g / slice_length as u32) as u8,
+            (average_b / slice_length as u32) as u8,
+            (average_a / slice_length as u32) as u8,
+        ]),
+        bounds,
+    }
+}
+
+#[allow(unused)]
+pub fn knn_colour(slice: &AverageSlice, k: usize) -> AverageSlice {
+    todo!()
+}
