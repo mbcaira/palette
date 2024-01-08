79e3dba59173fc355caf3193883dda359a8e2c89
Michael Caira <mbcaira@gmail.com>
2024-01-08 01:30:47 -0500

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,16 @@
 mod palette;
 
-use image::{io::Reader as ImageReader, ImageBuffer, Rgba};
-use palette::SliceType;
+use palette::{generate_palette, AveragingMethod, SliceType, SlicingConfig};
 
 fn main() {
-    let image_path = "image.png".to_string();
-    let img: image::DynamicImage = ImageReader::open(image_path)
-        .expect("Could not open image.")
-        .decode()
-        .expect("Could not decode image.");
-    let slices = palette::slice_image(&img, 4, SliceType::Horizontal);
-
-    let mut average = Vec::new();
-    for slice in slices {
-        average.push(palette::dominant_colour(slice))
-    }
-
-    let mut imgbuf: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::new(img.width(), img.height());
-    for (_, _, pixel) in imgbuf.enumerate_pixels_mut() {
-        *pixel = Rgba([0, 0, 0, 0])
-    }
-
-    for average_slice in average {
-        let x1 = average_slice.bounds.0;
-        let y1 = average_slice.bounds.1;
-        let x2 = average_slice.bounds.2;
-        let y2 = average_slice.bounds.3;
-
-        for pixel in imgbuf.enumerate_pixels_mut() {
-            let pixel_x = pixel.0;
-            let pixel_y = pixel.1;
-
-            let in_x = x1 <= pixel_x && pixel_x <= x2;
-            let in_y = y1 <= pixel_y && pixel_y <= y2;
-            if in_x && in_y {
-                *pixel.2 = average_slice.colour;
-            }
-        }
-    }
-
-    imgbuf.save("out.png").expect("Could not save image");
+    generate_palette(
+        AveragingMethod::Dominant,
+        "image.png".to_string(),
+        10,
+        "out.png".to_string(),
+        Some(SlicingConfig {
+            direction: SliceType::Horizontal,
+            cluster_size: None,
+        }),
+    )
 }
--- src/palette.rs
+++ src/palette.rs
@@ -1,19 +1,31 @@
-use image::{DynamicImage, GenericImageView, Rgba, SubImage};
+use image::{
+    io::Reader as ImageReader, DynamicImage, GenericImageView, ImageBuffer, Rgba, SubImage,
+};
+
+const DEFAULT_CLUSTER_SIZE: usize = 4;
 
 #[allow(unused)]
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum SliceType {
     Horizontal,
     Vertical,
 }
-
+pub enum AveragingMethod {
+    Dominant,
+    Cluster,
+}
 #[derive(Debug)]
 pub struct AverageSlice {
     pub colour: Rgba<u8>,
     pub bounds: (u32, u32, u32, u32),
 }
 
-pub fn slice_image(
+pub struct SlicingConfig {
+    pub direction: SliceType,
+    pub cluster_size: Option<usize>,
+}
+
+fn slice_image(
     img: &DynamicImage,
     num_sections: usize,
     slice_type: SliceType,
@@ -71,6 +83,61 @@ pub fn dominant_colour(slice: SubImage<&DynamicImage>) -> AverageSlice {
 }
 
 #[allow(unused)]
-pub fn knn_colour(slice: &AverageSlice, k: usize) -> AverageSlice {
+pub fn knn_colour(slice: SubImage<&DynamicImage>, k: usize) -> AverageSlice {
     todo!()
+}
+
+pub fn generate_palette(
+    method: AveragingMethod,
+    image_path: String,
+    num_sections: usize,
+    output_path: String,
+    slicing_config: Option<SlicingConfig>,
+) {
+    let img: image::DynamicImage = ImageReader::open(image_path)
+        .expect("Could not open image.")
+        .decode()
+        .expect("Could not decode image.");
+
+    let slice_type = match &slicing_config {
+        Some(config) => config.direction,
+        None => SliceType::Vertical,
+    };
+
+    let slices = slice_image(&img, num_sections, slice_type);
+    let mut average = Vec::new();
+    for slice in slices {
+        match method {
+            AveragingMethod::Dominant => average.push(dominant_colour(slice)),
+            AveragingMethod::Cluster => {
+                let cluster_size = match &slicing_config {
+                    Some(config) => config.cluster_size.unwrap_or(DEFAULT_CLUSTER_SIZE),
+                    None => DEFAULT_CLUSTER_SIZE,
+                };
+                average.push(knn_colour(slice, cluster_size))
+            }
+        }
+        average.push(dominant_colour(slice))
+    }
+
+    let mut imgbuf: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::new(img.width(), img.height());
+    for average_slice in average {
+        let x1 = average_slice.bounds.0;
+        let y1 = average_slice.bounds.1;
+        let x2 = average_slice.bounds.2;
+        let y2 = average_slice.bounds.3;
+
+        for pixel in imgbuf.enumerate_pixels_mut() {
+            let pixel_x = pixel.0;
+            let pixel_y = pixel.1;
+
+            let in_x = x1 <= pixel_x && pixel_x <= x2;
+            let in_y = y1 <= pixel_y && pixel_y <= y2;
+            if in_x && in_y {
+                *pixel.2 = average_slice.colour;
+            }
+        }
+    }
+
+    imgbuf.save(output_path).expect("Could not save image");
 }
